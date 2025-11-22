797305876ab8fb65a7ef7d5ffeeac09973685b24
Ithalo Araujo <araujoithalo@gmail.com>
2025-11-21 22:26:06 -0300

    feat: criando a tabela estadio

--- db/futebol.sql
+++ db/futebol.sql
@@ -1,7 +1,13 @@
--- Tabela PESSOA (Superclasse)
 CREATE TABLE Pessoa (
     cpf VARCHAR(1411) PRIMARY KEY,
     nome VARCHAR(999) NOT NULL,
     nacionalidade VARCHAR(50),
     data_nascimento DATE
+);
+
+CREATE TABLE Estadio (
+    id_estadio  SERIAL PRIMARY KEY,
+    nome        VARCHAR(999) NOT NULL,
+    cidade      VARCHAR(999),
+    capacidade  INTEGER
 );
