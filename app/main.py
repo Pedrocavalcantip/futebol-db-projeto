15e61d2d86b91536ad6fcbd724db91f80bc17a98
Ithalo Araujo <araujoithalo@gmail.com>
2025-11-22 09:58:19 -0300

    feat: comentando o codigo p/ conseguir explicar

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import os
-import psycopg
+import psycopg # Adaptador PostgreSQL para Python
 
 def main():
+
+    # Configurações de conexão
     host = os.getenv("DB_HOST", "localhost")
     port = os.getenv("DB_PORT", "5432")
     db   = os.getenv("DB_NAME", "futebol")
     user = os.getenv("DB_USER", "admin")
     pwd  = os.getenv("DB_PASSWORD", "admin123")
 
+    # Tentativa de conexão e tratamento de erros
     try:
         conn = psycopg.connect(
             host=host,
@@ -16,13 +19,16 @@ def main():
             user=user,
             password=pwd,
         )
+
         cur = conn.cursor()
         cur.execute("SELECT version();")
         version = cur.fetchone()[0]
         print(f"Conectado ao PostgreSQL: {version}")
+
         cur.close()
         conn.close()
         print("Conexão fechada.")
+
     except Exception as e:
         print(f"Falha ao conectar: {e}")
 
